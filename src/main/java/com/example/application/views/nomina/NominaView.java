f42433f8157456bb03a18e4866eb7489e93acae7
start.vaadin.com <no-reply@vaadin.com>
2026-04-11 00:50:31 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/nomina/NominaView.java
@@ -0,0 +1,36 @@
+package com.example.application.views.nomina;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Nomina")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.FILE)
+public class NominaView extends VerticalLayout {
+
+    public NominaView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
